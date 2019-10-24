b0b7cec60e5c539d47ff20c90f8ee3be0617e8c3
Patryk Sobow <patryk.sobow95@gmail.com>
2019-10-24 23:00:29 +0200

    Update lab2 scripts

--- lab2/dzien_tygodnia.sh
+++ lab2/dzien_tygodnia.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-	#W bashu białe znaki są ważne,np. podczas przypisania nie można dawać spacji obok znaku "="
-
-	# date +u% zwraca numer dnia tygodnia 1-7
+# W bashu białe znaki są ważne,np. podczas przypisania nie można dawać spacji obok znaku "="
+# date +u% zwraca numer dnia tygodnia 1-7
 dayNumber=$(date +%u)  
 
-test="witam test"
-
-	# za pomocą $ odwołujemy się do zawartości zmiennej nr_dnia_tygodnia
-if [ $dayNumber -ge 6 ]; then
+# za pomocą $ odwołujemy się do zawartości zmiennej nr_dnia_tygodnia
+if [[ $dayNumber -ge 6 ]]; 
+then
 	echo "It is weekend!"
 else
 	echo "It is workday"
 fi
-
-# nie rozumiem dlaczego tutaj musi być koniecznie pojedyncza klamra [] w wyrażeniu logicznym..
-# w dokumentacji zalecają używanie [[ ]] https://ss64.com/bash/test.html
 
